5b36bcef354f2ae6dbd4df0c4f6ef439d0854b4f
lamve2 <venice.lam@oocl.com>
2018-12-05 14:34:47 +0800

    change add new todo to Redux

---
+++ src/reducer/index.js
@@ -0,0 +1,12 @@
+const initialState ={
+    todos: []
+};
+export default (state = initialState, {type, payload}) => {
+    switch (type){
+        case "ADD_NEW_TODO":
+        return {todos: state.todos.concat(payload)};
+    default:
+        return state;
+    }
+    
+    }
