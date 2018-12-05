4a4b090ebf7a9786500bd287f1886700aa315c82
lamve2 <venice.lam@oocl.com>
2018-12-05 15:00:50 +0800

    refactor

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,27 +3,29 @@ import TodoInput from './TodoInput'
 import Todos from './Todos'
 import { connect } from "react-redux";
 
-class TodoList extends Component {
-  state = {todos: []}
-  addNewTodo = newTodo => {
-    // this.state.todos.push(newTodo)
-    // this.setState({todos: this.state.todos})
-    this.props.dispatch({
-      type: "ADD_NEW_TODO",
-      payload: newTodo
-    });
-  }
+export default class TodoList extends Component {
+  //state = {todos: []}
+  // addNewTodo = newTodo => {
+  //    this.state.todos.push(newTodo)
+  //    this.setState({todos: this.state.todos})
+  //   this.props.dispatch({
+  //     type: "ADD_NEW_TODO",
+  //     payload: newTodo
+  //   });
+  // }
   render() {
     return (
       <div>
-        <TodoInput onNewTodoAdded={this.addNewTodo}/>
-        <Todos todos={this.props.todos}/>
+        {/* <TodoInput onNewTodoAdded={this.addNewTodo}/>
+        <Todos todos={this.props.todos}/> */}
+        <TodoInput/>
+        <Todos/>
       </div>
     )
   }
 }
-const mapStateToProps = state => ({
-  todos: state.todos
-});
+// const mapStateToProps = state => ({
+//   todos: state.todos
+// });
 
-export default connect(mapStateToProps)(TodoList);
+// export default connect(mapStateToProps)(TodoList);
